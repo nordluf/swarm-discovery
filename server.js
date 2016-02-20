83892ff08eef51c512ddbe40b27ff2e08052c1b1
Evgeny Kruglov <ekruglov@gmail.com>
2016-02-21 00:55:15 +0100

    subscriber + recursive dns

---
+++ server.js
@@ -0,0 +1,174 @@
+"use strict";
+const _=require('lodash');
+const commander=require('commander');
+// :TODO add settings based on env vars and arguments
+commander
+  .version('0.0.1')
+  .usage('[OPTIONS] [ENDPOINT]:[PORT]')
+  .option('--debug','Logging more information')
+  .option('--network <name>','Multi-host default network name')
+  .option('--bind <bind>','Bind DNS server for this host:port')
+  .parse(process.argv);
+
+let docker;
+if (commander.args[0]) {
+  // so we can use http://apiurl:port
+  if (commander.args[0].match(/:\d+$/)){
+    let tmp=commander.args[0].lastIndexOf(':');
+    commander.args[1]=commander.args[0].slice(tmp+1);
+    commander.args[0]=commander.args[0].slice(0,tmp);
+  }
+
+  docker = new require('dockerode')({host: commander.args[0],port:commander.args[1]||2375});
+} else {
+  docker = new require('dockerode')();
+}
+
+const emitter = new (require('docker-events'))({docker});
+const dns=require('native-dns');
+const server = dns.createServer();
+
+const nodes={};
+
+emitter.on("connect", function() {
+  debug("Connected to docker api.");
+  let tmp=commander.bind?commander.bind.split(':'):[];
+  server.serve(tmp[1]||53,tmp[0]||'0.0.0.0');
+
+  docker.listContainers({},function(err,data){
+    if (err){
+      console.error(err.message);
+      return;
+    }
+    data.map(i=>addOne(i.Id,(new Date-1000)*1e6));
+  });
+});
+emitter.on("disconnect", function() {
+  // :TODO What can I do here?
+  console.error("Disconnected from docker api. Reconnecting.");
+});
+
+emitter.on('error',function(err){
+  console.error(err.message);
+});
+
+emitter.on("start", function(message) {
+  addOne(message.id,message.timeNano);
+  debug(`Container started: ${message.id}`);
+});
+emitter.on("die", function(message) {
+  removeOne(message.id,message.timeNano);
+  debug(`Container died: ${message.id}`);
+});
+emitter.on("pause", function(message) {
+  removeOne(message.id,message.timeNano);
+  debug(`Container paused: ${message.id}`);
+});
+emitter.on("unpause", function(message) {
+  addOne(message.id,message.timeNano);
+  debug(`Container unpaused: ${message.id}`);
+});
+
+emitter.start();
+
+console.log()
+
+server.on('request', function (req, res) {
+  if (dns.consts.QTYPE_TO_NAME[req.question[0].type]!='A'){
+    return dnsProxy(req,res);
+  }
+
+  console.log(req.question[0])
+  res.answer.push(dns.A({
+    name: req.question[0].name,
+    address: '192.168.122.1',
+    ttl: 600,
+  }));
+  res.answer.push(dns.A({
+    name: req.question[0].name,
+    address: '127.0.0.2',
+    ttl: 600,
+  }));
+  res.additional.push(dns.A({
+    name: 'hostA.example.org',
+    address: '127.0.0.3',
+    ttl: 600,
+  }));
+  res.send();
+});
+
+server.on('error', dnsErrorHandler);
+server.on('socketError', dnsErrorHandler);
+server.on('close',dnsErrorHandler);
+
+function addOne(id,nt){
+  docker.getContainer(id).inspect({},function(err,data){
+    if (err){
+      console.error(`Error ${err.statusCode}: '${err.reason}' for container ${id}`);
+      return;
+    }
+    if (!data || !data.State || !data.State.Running){
+      console.error(`Container ${id} not running`);
+      return;
+    }
+    if (data.State.Paused){
+      console.error(`Container ${id} paused`);
+      return;
+    }
+
+    let ip=null;
+    nodes[id]={
+      names:[id,data.Name.slice(1),data.Config.Hostname].map(i=>i.toLowerCase()),
+      netNames:_.mapValues(data.NetworkSettings.Networks,'Aliases'),
+      binds: _(data.NetworkSettings.Ports).map(v=>v&&v.map(v1=>(ip=v1.HostIp)+':'+v1.HostPort)).flatten().compact().value(),
+      ips:_.mapValues(data.NetworkSettings.Networks,'IPAddress'),
+      added: nt
+    };
+    nodes[id].ip=ip;
+    console.log(`Container ${id} added`);
+  });
+}
+
+function removeOne(id,nt){
+  if (!nodes[id]){
+    console.error(`Container ${id} not exists.`);
+    return;
+  }
+  if (nodes[id].added>=nt){
+    console.error(`Some action with ${id} already happened.`);
+    return;
+  }
+  delete nodes[id];
+  console.log(`Container ${id} removed`);
+}
+
+function dnsProxy(req,res){
+  let proxy=dns.Request({
+    question: req.question[0],
+    server: { address: '8.8.8.8', port: 53, type: 'udp' },
+    timeout: 500,
+  });
+
+  proxy.on('timeout', function () {
+    console.warn(`Timeout in making request for ${req.question[0].name}`);
+    res.cancel();
+  });
+  proxy.on('message', function (err, answer) {
+    res.answer.push(...answer.answer);
+    res.authority.push(...answer.authority);
+    res.additional.push(...answer.additional);
+  });
+  proxy.on('end', function () {
+    res.send();
+  });
+  proxy.send();
+}
+
+function dnsErrorHandler(err) {
+  console.error(err.message);
+  debug(err.stack);
+  process.exit();
+}
+function debug(msg){
+  commander.debug && console.warn(msg);
+}
